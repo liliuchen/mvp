403014b5302829097508949b3781d5d0dbf93130
李刘忱 <871898381@qq.com>
2018-04-27 16:48:09 +0800

    init

--- app/src/main/java/com/llc/framework/app/MyApplication.java
+++ app/src/main/java/com/llc/framework/app/MyApplication.java
@@ -26,7 +26,6 @@ public class MyApplication extends MultiDexApplication {
         super.onCreate();
         x.Ext.init(this);
         x.Ext.setDebug(BuildConfig.DEBUG); // 是否输出debug日志, 开启debug会影响性能.
-        Toast.makeText(this, "7654", Toast.LENGTH_SHORT).show();
 
     }
 
